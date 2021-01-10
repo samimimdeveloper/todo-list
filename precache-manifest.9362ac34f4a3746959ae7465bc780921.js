1cd0e2b314a5122bd163d7fd5f1622c760516790
mahdi <mahdisamimi.m@gmail.com>
2021-01-10 16:40:57 +0330

    Updates

---
+++ precache-manifest.9362ac34f4a3746959ae7465bc780921.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89348c89eabbc7d539864d3d2f98a45d",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "5a5aec582095b919ba7b",
+    "url": "/todo-list/static/js/2.813e7c85.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/todo-list/static/js/2.813e7c85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b0fbff584ee118aa2a6",
+    "url": "/todo-list/static/js/main.d99d5d01.chunk.js"
+  },
+  {
+    "revision": "6c2b3eea57a5e2ed8844",
+    "url": "/todo-list/static/js/runtime-main.9cbed67c.js"
+  }
+]);
